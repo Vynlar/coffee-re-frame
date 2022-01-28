a583407799b49fcea90577cc6a49974dc1232d50
Kolya Venturi <kolya.venturi@enx3s.com>
2022-01-28 11:44:16 -0700

    Update to actually generate CSS correctly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,5 @@
 module.exports = {
-  content: 
-    {
-      files: ['./src/**/*.{clj,cljs}'],
-      extract: {
-        cljs: content =>  {
-          const match = content.match(/\[:.+\s{:class\s"(.+?)"/); 
-          match && console.log(match);
-          return match || [];
-        },
-        clj: content => {
-          const match = content.match(/\[:.+{(?:.+)?:class\s+"(.+)"/);
-          if (match && match[1]) {
-            match[1] = match[1].split('.');
-          }
-
-          return match || [];
-        }
-      }
-    },
-  
+  content: ['./src/**/*.{clj,cljs}'],
   theme: {
     extend: {},
   },
