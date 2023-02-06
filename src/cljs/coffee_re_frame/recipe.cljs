d4218da19f31465e579af6fe4b3ec0708be33f38
Kolya Venturi <me@kolya.co>
2023-02-06 07:49:18 -0700

    Add newer V60 recipe (#19)
    * Add new v60 recipe
    
    * Resolve issues with recipe
    
    * Updates to prep

--- src/cljs/coffee_re_frame/recipe.cljs
+++ src/cljs/coffee_re_frame/recipe.cljs
@@ -32,16 +32,98 @@
 (s/def ::step (s/multi-spec step-type :step/type))
 (s/def ::recipe (s/keys :req [::name ::steps]))
 
+(defn create-v60-single-recipe [total-volume]
+  (let [coffee-weight (js/Math.floor (* total-volume 0.06))]
+    (def water-portion (* coffee-weight (/ 10 3)))
+    {::name "Hario v60"
+     ::steps [{:step/type :step.type/start
+               :step/title "Prepare"
+               :step/description (str "Boil about " (* 2 total-volume) "ml of water. Use some of the water to pre-heat your V60 brewer, without the filter. Grind " coffee-weight "g of coffee, set it aside for later. Place your filter into the v60 brewer.")}
+
+              {:step/type :step.type/prompt
+               :step/title "Rinse filter"
+               :step/description "Rinse filter with a generous amount of boiled water. Discard the water in the filter."}
+
+              {:step/type :step.type/prompt
+               :step/title "Add grounds"
+               :step/description (str "Add " coffee-weight "g coffee to the filter and use your finger to create a small hole in the center of the coffee. ")}
+
+              {:step/type :step.type/fixed
+               :step/title "Wet grounds"
+               :step/description (str "Gently pour " water-portion "ml of water into the middle of the gounds, spiraling outwards ensuring all grounds are wet.")
+               :step/volume (* total-volume (/ 50 250))
+               :step/duration 15
+               :step/timer :start}
+
+              {:step/type :step.type/fixed
+               :step/title "Bloom"
+               :step/description "Gently swirl the entire countainer to ensure grounds are wet, and wait for coffee to bloom, releasing CO2."
+               :step/duration 30}
+
+              {:step/type :step.type/fixed
+               :step/title "first pour"
+               :step/description "pour water at a strong, even rate, swirling from the center outwards. water should enter the grounds with force but not splash."
+               :step/volume (* total-volume (/ 50 250))
+               :step/duration 15}
+              
+              {:step/type :step.type/fixed
+               :step/title "1st Wait"
+               :step/description "Let the coffee rest"
+               :step/duration 10}
+
+              {:step/type :step.type/fixed
+               :step/title "Second pour"
+               :step/description "Continue to pour"
+               :step/volume (* total-volume (/ 50 250))
+               :step/duration 10}
+              
+              {:step/type :step.type/fixed
+               :step/title "2nd Wait"
+               :step/description "Let the coffee rest"
+               :step/duration 10}
+              
+              {:step/type :step.type/fixed
+               :step/title "Third pour"
+               :step/description "Continue to pour"
+               :step/volume (* total-volume (/ 50 250))
+               :step/duration 10}
+              
+              {:step/type :step.type/fixed
+               :step/title "3rd Wait"
+               :step/description "Let the coffee rest"
+               :step/duration 10}
+
+              {:step/type :step.type/fixed
+               :step/title "Final pour"
+               :step/description "Continue to pour"
+               :step/volume (* total-volume (/ 50 250))
+               :step/duration 10}
+
+              {:step/type :step.type/prompt
+               :step/title "Stir + swirl"
+               :step/description "Stir coffee and grounds with a spoon one turn in both directions, avoiding long-lasting swirling. Then pick up vessel and brewer and gently swirl several times."}
+
+              {:step/type :step.type/prompt
+               :step/title "Draw-down"
+               :step/display :step.display/time
+               :step/description "Wait for water level to meet the top of the grounds."}
+
+              {:step/type :step.type/end
+               :step/title "Enjoy"
+               :step/description "Discard coffee grounds and enjoy."
+               :step/display :step.display/time
+               :step/timer :stop}]}))
+
 (defn create-v60-recipe [total-volume]
   (let [coffee-weight (js/Math.floor (* total-volume 0.06))]
-    {::name "Hario v60"
+    {::name "Hario v60 (Multi-Cup)"
      ::steps [{:step/type :step.type/start
                :step/title "Prepare"
                :step/description (str "Grind " coffee-weight "g of coffee, set it aside for later. Place your filter into the v60 brewer.")}
 
               {:step/type :step.type/prompt
                :step/title "Rinse filter"
-               :step/description (str "Boil about " (* 1.5 total-volume) "ml of water. Rinse filter with a generous amount of boiled water. Discard the water.")}
+               :step/description (str "Boil about " (* 1.5 total-volume) "ml of water. Rinse filter with a generous amount of boiled water. Discard the water in the filter.")}
 
               {:step/type :step.type/prompt
                :step/title "Wet grounds"
@@ -83,7 +165,8 @@
                :step/timer :stop}]}))
 
 (def recipe-constructors
-  {:v60 create-v60-recipe})
+  {:v60-single create-v60-single-recipe
+  :v60 create-v60-recipe})
 
 (defn get-total-volume [recipe]
   (let [steps (::steps recipe)]
